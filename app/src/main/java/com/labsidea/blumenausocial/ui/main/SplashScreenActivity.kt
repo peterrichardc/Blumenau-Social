a143620e69822c55b0a414ddf74921b86e0ce57e
Peter Richard Carlim <Peter.richardcarlim@gmail.com>
2020-07-23 20:23:37 -0300

    Added Splash Screen Activity Kotlin

---
+++ app/src/main/java/com/labsidea/blumenausocial/ui/main/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+/*
+ * Author: Peter Richard Carlim.
+ * github: peterrichardc .
+ * e-mail: peter.richardcarlim@gmail.com .
+*/
+
+package com.labsidea.blumenausocial.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+
+    }
+}
